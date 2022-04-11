c7f7524e24c0662500fbf41b577d9533f42939c6
Baptiste Mnt <baptiste.menant@gmail.com>
2022-04-11 11:08:55 +0200

    Ajout de la croix pour la fermeture du menu < 950px

--- scripts/app.js
+++ scripts/app.js
@@ -7,4 +7,15 @@ let imgBtn = document.querySelector('.responsive-nav-btn img');
 
 btnNav.addEventListener('click', () => {
       listeNav.classList.toggle('active-nav');
+
+      if(imgBtn.src.includes('menu')){
+            imgBtn.src = './ressources/croix.svg';
+            imgBtn.style.width = "30px";
+            imgBtn.style.height = "30px";      
+      } else {
+            imgBtn.src = './ressources/menu.svg';
+            imgBtn.style.width = "40px";
+            imgBtn.style.height = "40px";  
+      }
+
 })
