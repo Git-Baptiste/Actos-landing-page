9061017b3b9ee4057392bf8fe14c86a4f5d37f25
Baptiste Mnt <baptiste.menant@gmail.com>
2022-04-12 18:33:24 +0200

    Ajout de la page tarifs + js + fix des liens corrompus

--- scripts/app.js
+++ scripts/app.js
@@ -62,3 +62,29 @@ allCross.forEach(logo => {
       })
 
 })
+
+
+// Onglets
+
+const choixTarifs = Array.from(document.querySelectorAll('.choix'));
+const contenuTarifs = Array.from(document.querySelectorAll('.panneau'));
+
+choixTarifs.forEach(choix => {
+      choix.addEventListener('click', (e) => {
+
+            let indexClic = choixTarifs.indexOf(e.target);
+
+            for(i = 0; i < choixTarifs.length; i++){
+
+                  if(i === indexClic){
+                        choixTarifs[i].classList.add('active-choix');
+                        contenuTarifs[i].classList.add('panneau-active');
+                  } else {
+                        choixTarifs[i].classList.remove('active-choix');
+                        contenuTarifs[i].classList.remove('panneau-active');
+                  }
+
+            }
+
+      })
+})
