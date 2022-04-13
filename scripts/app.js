cc9200c7f63ebc71a0f396ea6097f4089eee743c
Baptiste Mnt <baptiste.menant@gmail.com>
2022-04-13 10:21:14 +0200

    Fix du du 'minus' qui ne fonctionnait pas sur gitPages

--- scripts/app.js
+++ scripts/app.js
@@ -48,12 +48,12 @@ allCross.forEach(logo => {
             // console.log(height);
 
             if(this.src.includes('plus')){
-                  this.src = "/ressources/minus.svg";
+                  this.src = "./ressources/minus.svg";
                   currentChoice.style.height = height + 40 + "px";
                   currentChoice.style.opacity = 1;
                   currentChoice.style.padding = "20px 15px";
             } else {
-                  this.src = "/ressources/plus.svg";
+                  this.src = "./ressources/plus.svg";
                   currentChoice.style.height = 0;
                   currentChoice.style.opacity = 0;
                   currentChoice.style.padding = "0 15px";
